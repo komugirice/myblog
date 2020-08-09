0a7280a9027878bcd05aa579dc52137adb79a098
komugirice <iys60111118082@gmail.com>
2020-08-09 13:50:49 +0900

    add common.css

--- wp-content/themes/cocoon-child-master/functions.php
+++ wp-content/themes/cocoon-child-master/functions.php
@@ -4,4 +4,10 @@ if ( !defined( 'ABSPATH' ) ) exit;
 //子テーマ用のビジュアルエディタースタイルを適用
 add_editor_style();
 
+
 //以下に子テーマ用の関数を書く
+
+function wp_add_common_styles() {
+    wp_enqueue_style( 'common-css', get_stylesheet_directory_uri() . '/common.css' );
+}
+add_action( 'wp_enqueue_scripts', 'wp_add_common_styles' );
