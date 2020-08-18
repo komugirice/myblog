87cd5fb29e926b71535483acaf08fac2b56d9bec
komugirice <iys60111118082@gmail.com>
2020-08-18 15:06:59 +0900

    wp_make_content_images_responsive の使用はバージョン 5.5.0 から非推奨になっています ! 代わりに wp_filter_content_tags() を使ってください

--- wp-content/themes/cocoon-master/lib/page-settings/index-funcs.php
+++ wp-content/themes/cocoon-master/lib/page-settings/index-funcs.php
@@ -295,7 +295,6 @@ function get_category_index_list_entry_card_tag($categories, $count){
   // 昇順に変更する
   $args += array(
     'order' => 'ASC',
-    'orderby' => 'modified',
   );
   //カテゴリーの除外
   $exclude_category_ids = get_archive_exclude_category_ids();
